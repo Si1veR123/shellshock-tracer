5e180711356b31c23d76c704fcc644e626e6114a
Connor George <connorgeorgeem@aol.com>
2023-08-01 20:35:58 +0100

    move unnecessary items in config

--- src/event_loop.rs
+++ src/event_loop.rs
@@ -5,9 +5,8 @@ use winapi::shared::windef::{HWND, HBITMAP, HPEN};
 
 use crate::{
     Size,
-    bitmap::{
-        ARGB, Bitmap
-    },
+    Coordinate,
+    bitmap::Bitmap,
     WindowsMessageLoop,
     window_winapi::{
         draw_bitmap, screen_capture, bitmap_bits_to_buffer, draw_dotted_parametric_curve, object_cleanup
@@ -17,11 +16,6 @@ use crate::{
 };
 
 const LOOP_DURATION: Duration = Duration::from_millis(100);
-
-pub struct BitmapBuffers<'a> {
-    pub screen: Bitmap<'a, ARGB>,
-    pub score: Bitmap<'a, f32>,
-}
 
 /// The object handles must be exclusive pointers as they are deleted after use.
 pub struct WindowsObjects {
@@ -35,16 +29,19 @@ impl Drop for WindowsObjects {
     }
 }
 
-pub struct Config<'a> {
+pub struct Config {
     pub window_handle: HWND,
     pub shellshock_handle: HWND,
     pub dimensions: Size<u32>,
-    pub buffers: BitmapBuffers<'a>,
     pub windows_objects: WindowsObjects,
-    pub tank: Tank,
 }
 
-pub fn event_loop<'a>(mut cfg: Config<'a>) -> Result<(), String> {
+pub fn event_loop(cfg: Config) -> Result<(), String> {
+    let screen_buffer = Bitmap::new_static(cfg.dimensions, 0.into());
+    let mut score_buffer = Bitmap::new_static(cfg.dimensions, 0.0);
+
+    let mut tank = Tank { screen_position: Coordinate(0, 0), angle: -77, power: 37, wind: 23 };
+
     // Main windows message pump
     WindowsMessageLoop!(own_hwnd, LOOP_DURATION, {
         draw_bitmap(cfg.window_handle, cfg.windows_objects.bitmap, cfg.dimensions)
@@ -53,15 +50,15 @@ pub fn event_loop<'a>(mut cfg: Config<'a>) -> Result<(), String> {
         let screen_cap = screen_capture(cfg.shellshock_handle)
             .map_err(|err| format!("Error capturing screen: {err}"))?;
 
-        bitmap_bits_to_buffer(cfg.shellshock_handle, screen_cap, cfg.dimensions, cfg.buffers.screen.inner.as_mut_ptr())
+        bitmap_bits_to_buffer(cfg.shellshock_handle, screen_cap, cfg.dimensions, screen_buffer.inner.as_mut_ptr())
             .map_err(|err| format!("Error writing bitmap bits to buffer: {err}"))?;
 
-        let location = find_tank(&cfg.buffers.screen, &mut cfg.buffers.score)
+        let location = find_tank(&screen_buffer, &mut score_buffer)
             .ok_or_else(|| format!("Tank not found"))?;
 
-        cfg.tank.screen_position = location;
+        tank.screen_position = location;
 
-        let parametric_path = cfg.tank.construct_curve_function(cfg.dimensions);
+        let parametric_path = tank.construct_curve_function(cfg.dimensions);
 
         draw_dotted_parametric_curve(cfg.window_handle, cfg.windows_objects.bitmap, cfg.dimensions, cfg.windows_objects.pen, parametric_path)
             .map_err(|err| format!("Error drawing curve: {err}"))?;
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,22 @@
 use shellshock_tracer::window_winapi::{create_window, get_shellshock_window, create_dibitmap, window_dimensions, create_pen};
-use shellshock_tracer::Coordinate;
-use shellshock_tracer::bitmap::{ARGB, Bitmap};
-use shellshock_tracer::tank::Tank;
-use shellshock_tracer::event_loop::{BitmapBuffers, WindowsObjects, Config, event_loop};
+use shellshock_tracer::bitmap::ARGB;
+use shellshock_tracer::event_loop::{WindowsObjects, Config, event_loop};
 
 fn main() -> Result<(), String> {
     let own_hwnd = create_window().map_err(|err| format!("Error creating window: {err}"))?;
     let shellshock_hwnd = get_shellshock_window().ok_or("Shellshock application not found.")?;
     let dimensions = unsafe { window_dimensions(own_hwnd).expect("Failed to get window dimensions.") };
 
-    // Large buffers that has enough size to store the pixels of the screen.
-    let buffers = BitmapBuffers {
-        screen: Bitmap::new_static(dimensions, 0.into()),
-        score: Bitmap::new_static(dimensions, 0.0),
-    };
-
     let windows_objects = WindowsObjects {
         bitmap: unsafe { create_dibitmap(own_hwnd, dimensions, 0.into()).map_err(|_| "Error creating bitmap.")? },
         pen: unsafe { create_pen(2, ARGB { r: 200, b: 100, g: 100, a: 255 }) },
     };
 
-    let tank = Tank { screen_position: Coordinate(0, 0), angle: -77, power: 37, wind: 23 };
-
     let config = Config {
         window_handle: own_hwnd,
         shellshock_handle: shellshock_hwnd,
         dimensions,
-        buffers,
-        windows_objects,
-        tank,
+        windows_objects
     };
 
     event_loop(config)?;
